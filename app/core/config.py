16fc6d09455aab5f195fcf52ad68a3dd813f3f5d
dev-bong <qhddnjswo011@gmail.com>
2024-08-03 22:15:24 +0900

    🔧 각종 설정값 관리 모듈 추가
    - 데이터베이스 연결을 위해 .env 파일에서 설정값 읽는
    모듈 추가

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from starlette.config import Config
+from sqlalchemy import URL
+
+
+class Settings:
+    """
+    각종 설정 및 상수 관리
+    """
+
+    config = Config(".env")  # .env 파일 불러오기
+
+    DB_USER: str = config("DB_USER")
+    DB_PASSWORD: str = config("DB_PASSWORD")
+    DB_HOST: str = config("DB_HOST")
+    DB_DATABASE: str = config("DB_DATABASE")
+
+    def db_url_object(self):
+        return URL.create(
+            "postgresql+psycopg2",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            database=self.DB_DATABASE,
+        )
+
+
+settings = Settings()
